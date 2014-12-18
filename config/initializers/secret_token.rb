f630ff63a40b682e9106c54a1e33d056a6b9bfe5
Rick Casey <caseyrick@gmail.com>
2014-12-18 08:25:26 -0700

    Created application using Refinery CMS 2.1.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ricksplace::Application.config.secret_token = '1a797fae07100cf85597c15bf73cd8a051ba66e14308382309e38fbf6227423f2ebb35228872e7674046225dd6c0b0e65b8eb50f866fa393fa8a9447638fb333'
